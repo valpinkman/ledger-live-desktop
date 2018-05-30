a83a30a2507e11ffdd451af0ab43dcbcc3e9a539
Valentin D. Pinkman <valentin.d.pinkman@icloud.com>
2018-05-30 15:35:31 +0200

    fixed .env variable being undefined in commands

--- src/helpers/devices/getLatestFirmwareForDevice.js
+++ src/helpers/devices/getLatestFirmwareForDevice.js
@@ -4,8 +4,6 @@ import isEmpty from 'lodash/isEmpty'
 
 import getFirmwareInfo from './getFirmwareInfo'
 
-const { API_BASE_URL } = process.env
-
 type Input = {
   targetId: string | number,
   version: string,
@@ -18,15 +16,18 @@ export default async (data: Input) => {
 
     // Get device infos from targetId
     const { data: deviceVersion } = await axios.get(
-      `${API_BASE_URL}/device_versions_target_id/${data.targetId}`,
+      `${process.env.API_BASE_URL}/device_versions_target_id/${data.targetId}`,
     )
 
     // Fetch next possible firmware
-    const { data: serverData } = await axios.post(`${API_BASE_URL}/get_latest_firmware`, {
-      current_se_firmware_version: seFirmwareVersion.id,
-      device_version: deviceVersion.id,
-      providers: [1],
-    })
+    const { data: serverData } = await axios.post(
+      `${process.env.API_BASE_URL}/get_latest_firmware`,
+      {
+        current_se_firmware_version: seFirmwareVersion.id,
+        device_version: deviceVersion.id,
+        providers: [1],
+      },
+    )
 
     const { se_firmware_version } = serverData
 
